31583433af944e51313ef1a405dac3328f8d2c78
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-03 17:36:51 +0200

    Update blog-post-card.tsx

--- components/ui/blog-post-card.tsx
+++ components/ui/blog-post-card.tsx
@@ -1,45 +1,47 @@
-"use client";
-import { cn } from "@/lib/utils";
-import { IPost } from "@/models/Post";
+import { BlogPost } from "@/types/BlogPost";
 import Image from "next/image";
 
 interface CardProps {
-  data: Pick<IPost, "title" | "image" | "content" | "author" | "createdAt">;
+  data: BlogPost;
 }
 
 export function Card({ data }: CardProps) {
   return (
-    <div
-      className={cn(
-        "card backgroundImage relative mx-auto flex h-96 max-w-sm cursor-pointer flex-col justify-between overflow-hidden rounded-md p-4 shadow-xl",
-        `bg-[url(/upload/${data.image})] bg-cover`,
-      )}
-    >
-      <div className="absolute top-0 left-0 h-full w-full opacity-60 transition duration-300 group-hover/card:bg-black"></div>
-      <div className="z-10 flex flex-row items-center space-x-4">
-        <Image
-          height="100"
-          width="100"
-          alt="Avatar"
-          src="/logo.jpg"
-          className="h-10 w-10 rounded-full border-2 object-cover"
-        />
+    <div className="group/card w-full max-w-xs">
+      <div
+        className="card backgroundImage relative mx-auto flex h-96 max-w-sm cursor-pointer flex-col justify-between overflow-hidden rounded-md p-4 shadow-xl"
+        style={{
+          backgroundImage: `url(/upload/${data.image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="absolute top-0 left-0 h-full w-full opacity-60 transition duration-300 group-hover/card:bg-black"></div>
+        <div className="z-10 flex flex-row items-center space-x-4">
+          <Image
+            height="100"
+            width="100"
+            alt="Avatar"
+            src="/logo.jpg"
+            className="h-10 w-10 rounded-full border-2 object-cover"
+          />
+          <div className="flex flex-col">
+            <p className="relative z-10 text-base font-normal text-gray-50">
+              {data.author}
+            </p>
+            <p className="text-sm text-gray-400">
+              {new Date(data.createdAt).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
         <div className="flex flex-col">
-          <p className="relative z-10 text-base font-normal text-gray-50">
-            {data.author}
-          </p>
-          <p className="text-sm text-gray-400">
-            {new Date(data.createdAt).toLocaleDateString()}
+          <h3 className="relative z-10 text-xl font-bold text-gray-50 md:text-2xl">
+            {data.title}
+          </h3>
+          <p className="relative z-10 my-4 text-sm font-normal text-gray-50">
+            {data.content}
           </p>
         </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="relative z-10 text-xl font-bold text-gray-50 md:text-2xl">
-          {data.title}
-        </h3>
-        <p className="relative z-10 my-4 text-sm font-normal text-gray-50">
-          {data.content}
-        </p>
       </div>
     </div>
   );
