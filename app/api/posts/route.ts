36b1aaa2812627a7e557a6a34030eba579ce5b33
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-03 16:49:12 +0200

    Create route.ts

---
+++ app/api/posts/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import dbConnect from "../../../lib/dbConnect";
+import Post, { IPost } from "../../../models/Post";
+
+export async function GET() {
+  await dbConnect();
+
+  try {
+    const posts = await Post.find<IPost>({});
+    return NextResponse.json({ success: true, posts }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, error: "Failed to fetch posts" },
+      { status: 500 },
+    );
+  }
+}
