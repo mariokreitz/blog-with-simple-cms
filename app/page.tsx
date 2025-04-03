a6e0fb54ed6b8ca9e8e74ec857e961dbbf05a03e
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-03 16:49:14 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import ImagesSliderWrapper from "@/components/ImageSlider";
+import Posts, { getPosts } from "@/components/Posts";
+import { getQueryClient } from "@/lib/get-query-client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const App = () => {
+const App = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <ImagesSliderWrapper />
-    </>
+      <Posts />
+    </HydrationBoundary>
   );
 };
 
