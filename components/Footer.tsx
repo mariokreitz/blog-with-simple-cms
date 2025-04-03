69562a1a510e5b06e31fa6213e564e9797cff06b
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-03 11:51:42 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex h-24 flex-col items-center justify-center">
+      <p className="text-sm">
+        &copy; {new Date().getFullYear()} lipp.tattoos. Alle Rechte vorbehalten.
+      </p>
+      <div className="mt-2 flex space-x-4">
+        <Link href="/privacy-policy" className="text-sm underline">
+          Datenschutz
+        </Link>
+        <Link href="/imprint" className="text-sm underline">
+          Impressum
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
