6727a413c71cdcf97a044fe3c066f1c532cdb31f
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-03 17:20:53 +0200

    Update resizable-navbar.tsx

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -237,9 +237,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <Image src="/logo.jpg" alt="logo" width={30} height={30} />
-      <span className="font-medium text-black dark:text-white">
-        lipp.tattoos
-      </span>
+      <h1 className="font-medium text-black dark:text-white">lipp.tattoos</h1>
     </Link>
   );
 };
