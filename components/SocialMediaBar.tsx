bd0b9d9c25e36247f3a3f6d1f1feaff1f3b56d63
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-04 17:04:06 +0200

    Create SocialMediaBar.tsx

---
+++ components/SocialMediaBar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  IconBrandInstagram,
+  IconBrandWhatsapp,
+  IconBrandTiktok,
+} from "@tabler/icons-react";
+
+const SocialMediaBar = () => {
+  return (
+    <div className="fixed bottom-1/3 left-0 z-50 flex flex-col items-center justify-center gap-4 rounded-r-2xl bg-black p-3 shadow-sm shadow-gray-800">
+      <a
+        className="hover:text-[#DD2A7B] active:text-[#DD2A7B]"
+        href="https://www.instagram.com/lipp.tattoos/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconBrandInstagram />
+      </a>
+      <a
+        className="hover:text-green-500 active:text-green-500"
+        href="https://wa.me/4915123456789"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconBrandWhatsapp />
+      </a>
+
+      <a
+        className="hover:text-[#69c9d0] active:text-[#69c9d0]"
+        href="https://www.tiktok.com/@lipp.tattoos"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconBrandTiktok />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaBar;
