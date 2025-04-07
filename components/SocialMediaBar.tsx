2043abae937f39e0a6d310168663d3bcaf6813cf
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-07 09:23:10 +0200

    Update SocialMediaBar.tsx

--- components/SocialMediaBar.tsx
+++ components/SocialMediaBar.tsx
@@ -7,7 +7,7 @@ import {
 
 const SocialMediaBar = () => {
   return (
-    <div className="fixed bottom-1/3 left-0 z-50 flex flex-col items-center justify-center gap-4 rounded-r-2xl bg-black p-3 shadow-sm shadow-gray-800">
+    <div className="fixed bottom-1/4 left-0 z-50 flex flex-col items-center justify-center gap-4 rounded-r-2xl bg-black p-3 shadow-sm shadow-gray-800">
       <a
         className="hover:text-[#DD2A7B] active:text-[#DD2A7B]"
         href="https://www.instagram.com/lipp.tattoos/"
