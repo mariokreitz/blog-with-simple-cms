c07a806e85e61e54cb8b18b779b8b23123976b03
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-08 19:01:16 +0200

    Create page.tsx

---
+++ app/pages/privacy/page.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto px-4 py-24">
+      <h1 className="mb-4 text-4xl font-bold">Privacy Policy</h1>
+      <p className="mb-4">
+        Welcome to our Privacy Policy page. When you use our web services, you
+        trust us with your information. This Privacy Policy is meant to help you
+        understand what data we collect, why we collect it, and what we do with
+        it.
+      </p>
+      <h2 className="mb-2 text-2xl font-semibold">Information We Collect</h2>
+      <p className="mb-4">
+        We collect information to provide better services to all our users. This
+        information may include your name, email address, and other contact
+        details.
+      </p>
+      <h2 className="mb-2 text-2xl font-semibold">How We Use Information</h2>
+      <p className="mb-4">
+        We use the information we collect for purposes such as providing our
+        services, maintaining and improving our services, and developing new
+        services.
+      </p>
+      <h2 className="mb-2 text-2xl font-semibold">Information Sharing</h2>
+      <p className="mb-4">
+        We do not share personal information with companies, organizations, and
+        individuals outside of our company unless one of the following
+        circumstances applies: with your consent or for legal reasons.
+      </p>
+      <h2 className="mb-2 text-2xl font-semibold">Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, please contact us
+        at privacy@example.com.
+      </p>
+    </div>
+  );
+};
+
+export default page;
