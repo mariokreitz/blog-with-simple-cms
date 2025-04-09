44d34c5e8fd973253454129dd842258e2a47a1f5
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-09 13:07:45 +0200

    Update route.ts

--- app/pages/api/posts/route.ts
+++ app/pages/api/posts/route.ts
@@ -1,15 +1,12 @@
 import { NextResponse } from "next/server";
 import dbConnect from "../../../../lib/dbConnect";
 import Post from "../../../../schema/Post";
-import { BlogPost } from "../../../../types/BlogPost";
 
 export async function GET() {
   await dbConnect();
 
   try {
-    const posts = await Post.find<BlogPost>({})
-      .sort({ createdAt: -1 })
-      .limit(6);
+    const posts = await Post.find({}).sort({ createdAt: -1 }).limit(6);
     return NextResponse.json({ success: true, posts }, { status: 200 });
   } catch (error) {
     console.error(error);
