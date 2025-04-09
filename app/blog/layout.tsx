b111be66ec265f3d0b8d6b7d688a507777d4cfae
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-09 11:07:57 +0200

    Create layout.tsx

---
+++ app/blog/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Blog",
+  description: "Lipp Tattoo - Blog",
+};
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen">
+      <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  );
+}
