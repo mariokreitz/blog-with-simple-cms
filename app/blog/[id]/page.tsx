dff2517a2c87b68365466cc143aa8e9309933e6d
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-09 11:30:32 +0200

    Update page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -17,11 +17,35 @@ export default async function PostPage({
     if (!post) return notFound();
 
     return (
-      <div className="container mx-auto px-4 py-24">
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-        <p className="text-gray-600">{post.author}</p>
-        <p className="mt-4">{post.content}</p>
-      </div>
+      <article className="container mx-auto px-4 py-24">
+        <header className="mb-12">
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+          <p className="text-gray-600">
+            {new Date(post.createdAt).toLocaleDateString()}
+            {" - "}
+            {post.author}
+          </p>
+        </header>
+        <div className="mb-12 flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <div
+              style={{
+                backgroundImage: post.image
+                  ? `url(/upload/${post.image})`
+                  : `url(/fallback.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></div>
+          </div>
+          <div className="w-full md:w-1/2 md:pl-12">
+            <div
+              className="prose prose-lg"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
+          </div>
+        </div>
+      </article>
     );
   } catch (error) {
     console.error(error);
