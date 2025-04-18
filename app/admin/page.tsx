79551ec91e43428cf507d51b75e0fa47e82ae94e
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-18 15:40:23 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { authConfig } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import NewsManager from "@/components/NewsManager";
+import { SocialMediaManager } from "@/components/SocialMediaManager";
+import { EmailWhitelistManager } from "@/components/EmailWhitelistManager";
 
 const AdminPage = async () => {
   const session = await getServerSession(authConfig);
@@ -9,6 +12,9 @@ const AdminPage = async () => {
   return (
     <div className="container mx-auto space-y-8 px-4 py-24">
       <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+      <NewsManager />
+      <SocialMediaManager />
+      <EmailWhitelistManager />
     </div>
   );
 };
