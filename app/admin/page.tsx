10498431e10a7928def8cbde747619baf7325286
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-18 13:13:26 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return redirect("/");
+  }
   return (
     <div className="container mx-auto px-4 py-24">
       <h1>Admin</h1>
       <p>admin page works</p>
+      <h1>Welcome {session.user?.name}</h1>
+      <p>This is a protected dashboard page.</p>
     </div>
   );
 };
