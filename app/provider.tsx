113dca33ed59c4c7878ffc829eeab2a05a7839b0
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-18 13:15:32 +0200

    Update provider.tsx

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,11 +2,14 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "../lib/get-query-client";
+import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={getQueryClient()}>
-      {children}
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={getQueryClient()}>
+        {children}
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
