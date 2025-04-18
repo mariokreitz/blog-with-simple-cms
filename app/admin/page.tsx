4f79a5916d5cf5184f90cf44f7ea5d3164d4742a
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-18 13:46:01 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -17,38 +17,6 @@ const page = async () => {
         Beachte bitte, dass du nur Zugriff auf die Funktionen hast, die du
         brauchst, um die Website zu verwalten.
       </p>
-      <div className="mt-8 grid grid-cols-3 gap-4">
-        <div className="rounded-md bg-white p-4 shadow-md">
-          <h2 className="text-lg font-bold">Beiträge</h2>
-          <p className="text-sm">Liste aller Beiträge</p>
-          <a
-            href="/admin/posts"
-            className="text-blue-500 underline hover:text-blue-700"
-          >
-            Zur Beitragsübersicht
-          </a>
-        </div>
-        <div className="rounded-md bg-white p-4 shadow-md">
-          <h2 className="text-lg font-bold">Bilder</h2>
-          <p className="text-sm">Liste aller Bilder</p>
-          <a
-            href="/admin/images"
-            className="text-blue-500 underline hover:text-blue-700"
-          >
-            Zur Bilddateiübersicht
-          </a>
-        </div>
-        <div className="rounded-md bg-white p-4 shadow-md">
-          <h2 className="text-lg font-bold">Pages</h2>
-          <p className="text-sm">Liste aller Seiten</p>
-          <a
-            href="/admin/pages"
-            className="text-blue-500 underline hover:text-blue-700"
-          >
-            Zur Seitenübersicht
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
