16b21a19a6e3eb952ca3cc62ac10ae577a272a5a
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 10:53:24 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,7 @@ import NewsManager from "@/components/NewsManager";
 import { SocialMediaManager } from "@/components/SocialMediaManager";
 import { EmailWhitelistManager } from "@/components/EmailWhitelistManager";
 import { getAuthSession } from "@/auth";
+import ImageUploader from "@/components/ImageUploader";
 
 const AdminPage = async () => {
   const session = await getAuthSession();
@@ -11,6 +12,8 @@ const AdminPage = async () => {
   return (
     <div className="container mx-auto space-y-8 px-4 py-24">
       <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+      <h2>Image Uploader</h2>
+      <ImageUploader />
       <NewsManager />
       <SocialMediaManager />
       <EmailWhitelistManager />
