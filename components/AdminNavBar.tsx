aeb6c902ade30b6a0a0af7624aade5d3927adb3d
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 13:42:16 +0200

    Update AdminNavBar.tsx

--- components/AdminNavBar.tsx
+++ components/AdminNavBar.tsx
@@ -3,11 +3,29 @@ import React from "react";
 
 const AdminNavBar = () => {
   return (
-    <nav>
-      <Link href="/admin">Übersicht</Link>
-      <Link href="/admin/aws/images">Bilder</Link>
-      <Link href="/admin/news">Neue Post</Link>
-      <Link href="/admin/settings">Einstellungen</Link>
+    <nav className="p-4">
+      <div className="container mx-auto flex justify-between">
+        <div className="flex flex-wrap space-x-4">
+          <Link href="/admin" className="text-white hover:text-gray-300">
+            Übersicht
+          </Link>
+          <Link
+            href="/admin/aws/images"
+            className="text-white hover:text-gray-300"
+          >
+            Bilder
+          </Link>
+          <Link href="/admin/news" className="text-white hover:text-gray-300">
+            Neue Post
+          </Link>
+          <Link
+            href="/admin/settings"
+            className="text-white hover:text-gray-300"
+          >
+            Einstellungen
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
