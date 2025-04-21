b133cae6481254ffbf57acf6dfbd82d257ba37d1
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 12:03:33 +0200

    Update scrollToTop-button.tsx

--- components/ui/scrollToTop-button.tsx
+++ components/ui/scrollToTop-button.tsx
@@ -23,6 +23,8 @@ export const ScrollToTopButton = () => {
   }, []);
   return (
     <button
+      title="Scroll to top"
+      type="button"
       className={`${isVisible ? "" : "hidden"} fixed right-0 bottom-0 z-50 mb-[71px] flex cursor-pointer items-center gap-2 rounded-s-full bg-black px-4 py-2 text-xs shadow-sm shadow-gray-800`}
       onClick={scrollToTop}
     >
