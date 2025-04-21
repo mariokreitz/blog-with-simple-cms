50189d9ea800e212fc46ad6b88cd97995d7f9547
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 12:03:23 +0200

    Update next.config.ts

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,17 @@
+import AdminNavBar from "@/components/AdminNavBar";
+
 export const metadata = {
   title: "Lipp Tattoo - Admin",
   description: "Lipp Tattoo - CMS",
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="min-h-screen">{children}</main>;
+  return (
+    <main className="container mx-auto min-h-screen space-y-8 px-4 py-24">
+      <AdminNavBar />
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
