841fcf92948bcc877503e5829ad6784b4ce95a42
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 12:47:10 +0200

    Update AdminNavBar.tsx

--- components/AdminNavBar.tsx
+++ components/AdminNavBar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const AdminNavBar = () => {
-  return <div>AdminNavBar</div>;
+  return (
+    <nav>
+      <Link href="/admin">Übersicht</Link>
+      <Link href="/admin/aws/images">Bilder</Link>
+    </nav>
+  );
 };
 
 export default AdminNavBar;
