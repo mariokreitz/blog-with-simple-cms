3072bddab1a703fd98f8cf8ed204126c333e1e2e
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-21 12:03:35 +0200

    Update dataFetching.ts

--- lib/dataFetching.ts
+++ lib/dataFetching.ts
@@ -1,7 +1,13 @@
-import { BlogPost } from "@/types/BlogPost";
+import type { BlogPost } from "@/types/BlogPost";
+import type { ImageData } from "@/types/ImageData";
 import axios from "axios";
 
 export const getPosts = async (): Promise<BlogPost[]> => {
   const response = await axios.get("/api/posts");
   return response.data.posts;
+};
+
+export const getImages = async (): Promise<ImageData[]> => {
+  const response = await axios.get("/api/aws/images");
+  return response.data.images;
 };
