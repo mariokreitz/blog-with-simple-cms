bbee17a0ab31b98c1b02ae8cda5bf079547a2530
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-22 13:07:50 +0200

    Update route.ts

--- app/api/social-links/route.ts
+++ app/api/social-links/route.ts
@@ -1,18 +1,22 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/mongoose";
 import SocialLinks from "@/models/SocialLinks";
 
 export async function GET() {
   await dbConnect();
-  const record = await SocialLinks.findOne();
-  return NextResponse.json(record || {});
+  const record = await SocialLinks.find().lean();
+  console.log(record);
+  return NextResponse.json(record[0]?.links || {});
 }
-export async function PUT(req: Request) {
+
+export async function PUT(request: NextRequest) {
   await dbConnect();
-  const data = await req.json();
-  const updated = await SocialLinks.findOneAndUpdate({}, data, {
-    upsert: true,
-    new: true,
-  });
+  const data = await request.json();
+
+  const updated = await SocialLinks.findOneAndUpdate(
+    { links: data },
+    { upsert: true, new: true },
+  );
+
   return NextResponse.json(updated);
 }
