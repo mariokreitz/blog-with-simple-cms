f1e817d1784162d3df64128526977a5358a117a6
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-22 13:57:10 +0200

    Update page.tsx

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <>
+    <div className="grid grid-cols-1 gap-6 px-4 py-8 lg:grid-cols-2">
       <SocialMediaManager />
       <EmailWhitelistManager />
-    </>
+    </div>
   );
 };
 
