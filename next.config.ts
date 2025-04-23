725c57d2538c70461ad4977842c625d472500a68
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-24 01:56:45 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lipptattoo.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
