00177251250f51942da275421c868a5b071390af
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-25 22:30:34 +0200

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,17 @@
+import { getAuthSession } from "@/auth";
 import AdminNavBar from "@/components/AdminNavBar";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Lipp Tattoo - Admin",
   description: "Lipp Tattoo - CMS",
 };
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getAuthSession();
+  if (!session) {
+    redirect("/");
+  }
   return (
     <main className="container mx-auto min-h-screen space-y-8 px-4 py-24">
       <AdminNavBar />
