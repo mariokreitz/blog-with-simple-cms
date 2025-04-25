0129a8b497e637fd45cf0c1d7b11b971f8671cb5
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-25 22:30:36 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { redirect } from "next/navigation";
-import { getAuthSession } from "@/auth";
 
 const AdminPage = async () => {
-  const session = await getAuthSession();
-  if (!session) redirect("/");
   return (
     <>
       <h1 className="text-3xl font-bold">Adminpanel</h1>
